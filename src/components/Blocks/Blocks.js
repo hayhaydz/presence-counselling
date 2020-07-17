f3b98565dff447e50d8b2ef1285fcf1c08d3eec3
hayhaydz <haydon1900@gmail.com>
2020-07-17 11:44:52 +0100

    Refined Experience text and swapped images

--- src/components/Blocks/Blocks.js
+++ src/components/Blocks/Blocks.js
@@ -6,19 +6,18 @@ import BlueSkyGreenFieldImg from "../../img/blue-sky-green-field.jpg"
 import StonehengeImg from "../../img/stonehenge.jpg"
 import RunningWater from "../../img/running-water.jpg"
 import WoodlandPath from "../../img/woodland-path.jpg"
-import WaterfallWoodland from "../../img/waterfall-woodland.jpg"
 
 const Blocks = () => {
   return (
     <section className="Blocks">
       <h1 className="Blocks__heading">What can I do for you?</h1>
       <Block
-        img={BlueSkyGreenFieldImg}
+        img={StonehengeImg}
         alt="Blue sky with large whispy clouds above a green field wiht trees and hedges"
         text="Are you struggling with how to keep your head above the waves? Wanting to see new possibilities for your life? Perhaps you are concerned about your child? I have experience working with adults and teenagers and am able to build a nurturing relationship using care and respect. I believe it is the relationship between therapist and client that brings healing."
       />
       <Block
-        img={StonehengeImg}
+        img={BlueSkyGreenFieldImg}
         alt="Large boulder in the foreground with stonehenge in the distance behind"
         text="I can offer a safe presence in your time of need. I’m a warm and empathic listener able to offer hope and interested in accompanying you through whatever issue you face. I like to work with you as a whole person; often strong emotions stem from something that has happened to you."
       />
@@ -31,7 +30,7 @@ const Blocks = () => {
       <Block
         img={WoodlandPath}
         alt=""
-        text="For over 20 years I have worked with adolescents in various contexts:  As a mediator when they have been at risk of homelessness when the relationship between parent and young person has fractured. I have offered emotional support for young people aged 16-25 including for those struggling with substance addiction. I have 7 years counselling adolescents face to face and online. I have worked for 5 years counselling adults face to face."
+        text="For over twenty years I have worked with adolescents in various contexts: As a mediator when young people are at risk of homelessness. When the relationship between parent and young person has fractured. I have offered emotional support for young people aged 16-25; including for those struggling with substance addiction. I have been counselling adolescents for seven years; face to face and online. I have been counselling adults face to face for five years."
       />
     </section>
   )
